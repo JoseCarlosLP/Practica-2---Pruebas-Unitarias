20627f910906815b206f33884cddfb65638a14da
Jose Carlos <jose.lozada@ucb.edu.bo>
2024-10-18 17:23:40 -0400

    Se agrego pre y postcondiciones a la prueba unitaria, junto con varios asserts

--- test/test_movrey.py
+++ test/test_movrey.py
@@ -1,51 +1,43 @@
+from unittest import mock
 import pytest
-from ajedrezoo import metapieza
-from unittest.mock import MagicMock, patch
+from ajedrezoo import metarey
+from unittest.mock import MagicMock
 
 
 class TestMovrey:
 
     @pytest.fixture
     def setup_rey(self):
-        from ajedrezoo import metarey
+        # Inicializamos el rey en su posicion inical
         return metarey(5, 1, 1)
 
-    @patch('ajedrezoo.torrenegra')
-    @patch('ajedrezoo.torreblanca')
-    def test_rey_movido_sin_enroque(self, mock_torreblanca, mock_torrenegra, setup_rey):
+    @mock.patch('ajedrezoo.torrenegra')
+    @mock.patch('ajedrezoo.torreblanca')
+    @mock.patch('ajedrezoo.metapieza.movlineal', return_value=[])
+    @mock.patch('ajedrezoo.metapieza.movdiagonal', return_value=[])
+    def test_rey_movido_sin_enroque(self, mock_movlineal, mock_diagonal, mock_torreblanca, mock_torrenegra, setup_rey):
+        rey = setup_rey
         torres_blancas = [0, MagicMock(movida=0), MagicMock(movida=0)]
         torres_negras = [0, MagicMock(movida=0), MagicMock(movida=0)]
 
         mock_torrenegra.__getitem__.side_effect = torres_negras.__getitem__
         mock_torreblanca.__getitem__.side_effect = torres_blancas.__getitem__
+        # Precondicion: Las torres y el rey no se movieron
+        assert mock_torrenegra[1].movida == 0
+        assert mock_torrenegra[2].movida == 0
+        assert mock_torreblanca[1].movida == 0
+        assert mock_torreblanca[2].movida == 0
+        assert rey.movida == 0
+        # Precondicion: El rey no puede moverse a ninguna casilla
+        assert rey.movdiagonal() == []
+        assert rey.movlineal() == []
 
-        rey = setup_rey
         rey.movida = 1  # Avisamos que el rey se movió
+        mov_posibles = rey.movrey()
 
-        # Mockeamos las llamadas a otras funciones que tiene la funcion y especificamos el valor a retornar
-        metapieza.movdiagonal = MagicMock(return_value=[])
-        metapieza.movlineal = MagicMock(return_value=[])
+        #Postcondicion: El rey no puede enrocar porque se movió
+        assert rey.movida == 1
+        assert mov_posibles == []
+        assert rey.casposibles == []
 
-        assert rey.movrey() == []
 
-    @patch('ajedrezoo.torrenegra')
-    @patch('ajedrezoo.torreblanca')
-    @patch('ajedrezoo.cocupadas')
-    def test_no_puede_enrocar_a_ningun_lado(self, mock_cocupadas, mock_torreblanca, mock_torrenegra, setup_rey):
-        torres_blancas = [0, MagicMock(movida=1), MagicMock(movida=0)]
-        torres_negras = [0, MagicMock(movida=1), MagicMock(movida=0)]
-
-        mock_torrenegra.__getitem__.side_effect = torres_negras.__getitem__
-        mock_torreblanca.__getitem__.side_effect = torres_blancas.__getitem__
-
-        rey = setup_rey
-        # Mockeamos las llamadas a otras funciones que tiene la funcion y especificamos el valor a retornar
-        metapieza.movdiagonal = MagicMock(return_value=[])
-        metapieza.movlineal = MagicMock(return_value=[])
-
-        matriz_cocupadas = [[0] * 8 for _ in range(8)]
-        matriz_cocupadas[rey.casy][rey.casx + 2] = 1
-
-        mock_cocupadas.__getitem__.side_effect = matriz_cocupadas.__getitem__
-
-        assert rey.movrey() == []
