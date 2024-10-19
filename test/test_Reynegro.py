1ba9dad1ec52bdba6d2f60cb55c6f64a6469346f
Jose Carlos <jose.lozada@ucb.edu.bo>
2024-10-19 11:56:01 -0400

    Se implemento la prueba unitaria que verifica que el Reynegro solo puede moverse a las casillas que el metodo metarey.movrey devuelve

--- test/test_Reynegro.py
+++ test/test_Reynegro.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 import pygame
 
 from ajedrezoo import Reynegro
@@ -11,3 +13,14 @@ class TestReynegro:
         assert pieza.casy == 1
         assert pieza.color == 2
         assert isinstance(pieza.foto, pygame.Surface)
+
+    @mock.patch('ajedrezoo.metarey.movrey', return_value=[(4, 1), (6, 1), (5, 2), (4, 2), (6, 2)])
+    def test_puedemovera(self, mock_movdiagonal):
+        pieza = Reynegro()
+        # Precondicion: Que la instancia del Reynegro sea correcta
+        assert isinstance(pieza, Reynegro)
+
+        mov_posibles = pieza.puedemovera()
+
+        # Postcondicion: Solo se puede mover a las casillas que el metodo metarey.movrey devuelve
+        assert mov_posibles == [(4, 1), (6, 1), (5, 2), (4, 2), (6, 2)]
