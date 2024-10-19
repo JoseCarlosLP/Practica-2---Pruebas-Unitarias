b63eda8c124f7a6840f6be5d1b7a3c0f6b1a5212
Jose Carlos <jose.lozada@ucb.edu.bo>
2024-10-19 12:02:33 -0400

    Se implemento la prueba unitaria que verifica que el Reyblanco solo puede moverse a las casillas que el metodo metarey.movrey devuelve

--- test/test_Reyblanco.py
+++ test/test_Reyblanco.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 import pygame
 
 from ajedrezoo import Reyblanco
@@ -11,3 +13,14 @@ class TestReyblanco:
         assert pieza.casy == 8
         assert pieza.color == 1
         assert isinstance(pieza.foto, pygame.Surface)
+
+    @mock.patch('ajedrezoo.metarey.movrey', return_value=[(4, 8), (6, 8), (5, 7), (4, 7), (6, 7)])
+    def test_puedemovera(self, mock_movdiagonal):
+        pieza = Reyblanco()
+        # Precondicion: Que la instancia del Reyblanco sea correcta
+        assert isinstance(pieza, Reyblanco)
+
+        mov_posibles = pieza.puedemovera()
+
+        # Postcondicion: Solo se puede mover a las casillas que el metodo metarey.movrey devuelve
+        assert mov_posibles == [(4, 8), (6, 8), (5, 7), (4, 7), (6, 7)]
