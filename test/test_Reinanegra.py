7b0b00e81ad6f85545d5d9cbb8baec05996fc70b
Jose Carlos <jose.lozada@ucb.edu.bo>
2024-10-19 12:15:55 -0400

    Se implemento la prueba unitaria que verifica que la Reinanegra solo puede moverse a las casillas que los metodos movlineal y movdiagonal devuelven

--- test/test_Reinanegra.py
+++ test/test_Reinanegra.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 import pygame
 
 from ajedrezoo import Reinanegra
@@ -11,3 +13,15 @@ class TestReinanegra:
         assert pieza.casy == 1
         assert pieza.color == 2
         assert isinstance(pieza.foto, pygame.Surface)
+
+    @mock.patch('ajedrezoo.metapieza.movlineal', return_value=[(4, 2)])
+    @mock.patch('ajedrezoo.metapieza.movdiagonal', return_value=[(3, 2)])
+    def test_puedemovera(self, mock_movdiagonal, mock_movlineal):
+        pieza = Reinanegra()
+        # Precondicion: Que la instancia del Reinanegra sea correcta
+        assert isinstance(pieza, Reinanegra)
+
+        mov_posibles = pieza.puedemovera()
+
+        # Postcondicion: Solo se puede mover a las casillas que los metodos movlineal y movdiagonal devuelve
+        assert mov_posibles == [(4, 2), (3, 2)]
