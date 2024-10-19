59afed7979a2231d42231448e941209e9c4c3039
unknown <isabqm121@gmail.com>
2024-10-19 16:16:34 -0400

    Implementando test para Torrenegra.puedemovera()

--- test/test_torrenegra.py
+++ test/test_torrenegra.py
@@ -1,6 +1,7 @@
 import pytest
-from ajedrezoo import Torrenegra, ocupadas, cocupadas
+from ajedrezoo import *
 import pygame
+from unittest import mock
 
 @pytest.fixture
 def setup_teardown_torre_negra():
@@ -23,12 +24,13 @@ def setup_teardown_torre_negra():
     
 class TestTorreNegraInit:
     
-    def test_crear_torre(self, setup_teardown_torre_negra):
+    def test_torrenegra_init_(self, setup_teardown_torre_negra):
         
         x=8
         # Precondición:
         assert 0 <= x < 9
         ob = Torrenegra(x)
+        print(ob.puedemovera())
         
         # Postcondición:
         assert ob.casx < 9
@@ -37,3 +39,16 @@ class TestTorreNegraInit:
         assert isinstance(ob.foto, pygame.Surface)
         assert ocupadas[ob.casy][ob.casx] == ob
         assert cocupadas[ob.casy][ob.casx] == ob.color
+    @mock.patch('ajedrezoo.metapieza.movlineal', return_value=[(7, 1), (8, 2),(6,1)])
+    def test_puedemovera(self, mock_movlineal):
+        x=8
+        # Precondición:
+        assert 0 <= x < 9
+        ob = Torrenegra(x)
+        # Precondicion: Que la instancia del Alfilnegro sea correcta
+        assert isinstance(ob, Torrenegra)
+
+        mov_posibles = ob.puedemovera()
+
+        #Postcondicion: Solo se puede mover a las casillas que el metodo metapieza.movdiagonal devuelve
+        assert mov_posibles == [(7, 1), (8, 2),(6,1)]
