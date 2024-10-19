11c3194b405ba311e3ee7a3ebb31844d1cb995f7
Jose Carlos <jose.lozada@ucb.edu.bo>
2024-10-19 15:02:32 -0400

    Se implemento la prueba unitaria para verificar el correcto funcionamiento de la funcion sacapieza, la cual obtiene una pieza en la posicion x,y del tablero

--- test/test_Alfilnegro.py
+++ test/test_Alfilnegro.py
@@ -1,5 +1,4 @@
 from unittest import mock
-from unittest.mock import MagicMock
 
 import pygame
 import pytest
--- test/test_sacapieza.py
+++ test/test_sacapieza.py
@@ -1,8 +1,6 @@
-from unittest.mock import MagicMock
-
 import pytest
 
-from ajedrezoo import Reinablanca, comepieza, ocupadas, cocupadas
+from ajedrezoo import ocupadas, cocupadas, sacapieza, Reyblanco
 
 
 @pytest.fixture
@@ -25,27 +23,18 @@ def setup_teardown_metapieza():
         all(casilla == 0 for casilla in fila) for fila in cocupadas), "Error: cocupadas no fue limpiada correctamente"
 
 
-def test_comepieza( setup_teardown_metapieza):
+def test_sacapieza(setup_teardown_metapieza):
     # Precondicion: Instanciamos la pieza y verificamos que sus propiedades sean correctas y este en el tablero
-    pieza = Reinablanca()
+    pieza = Reyblanco()
     assert pieza.movida == 0
-    assert pieza.casx == 4
+    assert pieza.casx == 5
     assert pieza.casy == 8
     assert ocupadas[pieza.casy][pieza.casx] == pieza
-    assert cocupadas[pieza.casy][pieza.casx] == 1
 
-    def mock_cambiasilla(x, y):
-        ocupadas[pieza.casy][pieza.casx] = cocupadas[pieza.casy][pieza.casx] = 0
-        pieza.casx = x
-        pieza.casy = y
-        pieza.movida = 1
+    resultado = sacapieza(5, 8)
 
-    pieza.cambiasilla = MagicMock(side_effect=mock_cambiasilla)
-    comepieza(pieza)
-
-    # Postcondicion: La pieza ha sido removida del tablero y la casilla que estaba ocupando se encuentra libre
-    assert pieza.movida == 1
-    assert pieza.casx == 9
-    assert pieza.casy == 9
-    assert ocupadas[8][4] == 0
-    assert cocupadas[8][4] == 0
+    # Postcondicion: La pieza resultado obtenida de sacar la pieza del tablero, debe ser la misma que se instancio
+    assert resultado is pieza
+    assert resultado.movida == 0
+    assert resultado.casx == 5
+    assert resultado.casy == 8
