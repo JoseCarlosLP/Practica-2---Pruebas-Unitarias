105bdb7c3991a1a08dcea55a9d422dd833545f12
Jose Carlos <jose.lozada@ucb.edu.bo>
2024-10-19 11:34:49 -0400

    Se implemento la prueba unitaria que verifica que el Alfilblanco solo puede moverse a las casillas que el metodo metapieza.movdiagonal devuelve

--- test/test_Alfilblanco.py
+++ test/test_Alfilblanco.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 import pygame
 
 from ajedrezoo import Alfilblanco
@@ -12,3 +14,14 @@ class TestAlfilblanco:
         assert pieza.casy == 8
         assert pieza.color == 1
         assert isinstance(pieza.foto, pygame.Surface)
+
+    @mock.patch('ajedrezoo.metapieza.movdiagonal', return_value=[(2, 7), (4, 7)])
+    def test_puedemovera(self, mock_movdiagonal):
+        pieza = Alfilblanco(3)
+        # Precondicion: Que la instancia del Alfilblanco sea correcta
+        assert isinstance(pieza, Alfilblanco)
+
+        mov_posibles = pieza.puedemovera()
+
+        # Postcondicion: Solo se puede mover a las casillas que el metodo metapieza.movdiagonal devuelve
+        assert mov_posibles == [(2, 7), (4, 7)]
