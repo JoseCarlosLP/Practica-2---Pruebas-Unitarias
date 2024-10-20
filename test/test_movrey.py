8b4fd00ffcdbc0ab08e9014d4f850d1910e1e4d7
Jose Carlos <jose.lozada@ucb.edu.bo>
2024-10-20 17:19:35 -0400

    Se cambio el fixture del archivo test_movrey

--- test/test_movrey.py
+++ test/test_movrey.py
@@ -7,16 +7,29 @@ from unittest.mock import MagicMock
 class TestMovrey:
 
     @pytest.fixture
-    def setup_rey(self):
-        # Inicializamos el rey en su posicion inical
-        return metarey(5, 1, 1)
+    def setup_teardown_rey(self):
+        print("\nInicializando rey...")
+        rey = metarey(5, 1, 1)
+        yield rey
+        print("Limpiando rey...")
+
+    @pytest.fixture
+    def setup_teardown_cocupadas(self):
+        print("Inicializando cocupadas...")
+        matriz_cocupadas = [[0] * 8 for _ in range(8)]
+        yield matriz_cocupadas
+        print("\nLimpiando cocupadas...")
+        matriz_cocupadas = [[0] * 8 for _ in range(8)]
+        assert all(
+            all(casilla == 0 for casilla in fila) for fila in matriz_cocupadas), "Error: cocupadas no fue limpiada correctamente"
 
     @mock.patch('ajedrezoo.torrenegra')
     @mock.patch('ajedrezoo.torreblanca')
     @mock.patch('ajedrezoo.metapieza.movlineal', return_value=[])
     @mock.patch('ajedrezoo.metapieza.movdiagonal', return_value=[])
-    def test_rey_movido_sin_enroque(self, mock_movlineal, mock_diagonal, mock_torreblanca, mock_torrenegra, setup_rey):
-        rey = setup_rey
+    def test_rey_movido_sin_enroque(self, mock_movlineal, mock_diagonal, mock_torreblanca, mock_torrenegra,
+                                    setup_teardown_rey):
+        rey = setup_teardown_rey
         torres_blancas = [0, MagicMock(movida=0), MagicMock(movida=0)]
         torres_negras = [0, MagicMock(movida=0), MagicMock(movida=0)]
 
@@ -46,14 +59,15 @@ class TestMovrey:
     @mock.patch('ajedrezoo.metapieza.movlineal', return_value=[])
     @mock.patch('ajedrezoo.metapieza.movdiagonal', return_value=[])
     def test_no_puede_enrocar_a_ningun_lado(self, mock_movlineal, mock_diagonal, mock_torreblanca, mock_torrenegra,
-                                            mock_cocupadas, setup_rey):
-        rey = setup_rey
+                                            mock_cocupadas, setup_teardown_rey, setup_teardown_cocupadas):
+        rey = setup_teardown_rey
         torres_blancas = [0, MagicMock(movida=1), MagicMock(movida=0)]
         torres_negras = [0, MagicMock(movida=1), MagicMock(movida=0)]
         mock_torrenegra.__getitem__.side_effect = torres_negras.__getitem__
         mock_torreblanca.__getitem__.side_effect = torres_blancas.__getitem__
 
-        mock_cocupadas = [[0] * 8 for _ in range(8)]
+        matriz_cocupadas = setup_teardown_cocupadas
+        mock_cocupadas.__getitem__.side_effect = matriz_cocupadas.__getitem__
         mock_cocupadas[rey.casy][rey.casx + 2] = 1
 
         # Precondicion: Las torres en el lado izquierdo del tablero se movieron, las torres en el lado derecho y el
@@ -79,8 +93,8 @@ class TestMovrey:
     @mock.patch('ajedrezoo.metapieza.movlineal', return_value=[])
     @mock.patch('ajedrezoo.metapieza.movdiagonal', return_value=[])
     def test_todas_las_torres_movidas(self, mock_movlineal, mock_diagonal, mock_torreblanca, mock_torrenegra,
-                                      setup_rey):
-        rey = setup_rey
+                                      setup_teardown_rey):
+        rey = setup_teardown_rey
         torres_blancas = [0, MagicMock(movida=1), MagicMock(movida=1)]
         torres_negras = [0, MagicMock(movida=1), MagicMock(movida=1)]
         mock_torrenegra.__getitem__.side_effect = torres_negras.__getitem__
@@ -107,14 +121,16 @@ class TestMovrey:
     @mock.patch('ajedrezoo.metapieza.movdiagonal', return_value=[])
     def test_sin_enroque_torres_izq_movidas_casillas_dere_ocupadas(self, mock_movlineal, mock_diagonal,
                                                                    mock_torreblanca,
-                                                                   mock_torrenegra, mock_cocupadas, setup_rey):
-        rey = setup_rey
+                                                                   mock_torrenegra, mock_cocupadas, setup_teardown_rey,
+                                                                   setup_teardown_cocupadas):
+        rey = setup_teardown_rey
         torres_blancas = [0, MagicMock(movida=1), MagicMock(movida=0)]
         torres_negras = [0, MagicMock(movida=1), MagicMock(movida=1)]
         mock_torrenegra.__getitem__.side_effect = torres_negras.__getitem__
         mock_torreblanca.__getitem__.side_effect = torres_blancas.__getitem__
 
-        mock_cocupadas = [[0] * 8 for _ in range(8)]
+        matriz_cocupadas = setup_teardown_cocupadas
+        mock_cocupadas.__getitem__.side_effect = matriz_cocupadas.__getitem__
         mock_cocupadas[rey.casy][rey.casx + 2] = 1
 
         # Precondicion: Las torres en el lado izquierdo del tablero se movieron, las torres en el lado derecho y el
@@ -142,14 +158,17 @@ class TestMovrey:
     @mock.patch('ajedrezoo.metapieza.movdiagonal', return_value=[])
     def test_sin_enroque_torres_izq_movidas_una_casilla_dere_ocupada(self, mock_movlineal, mock_diagonal,
                                                                      mock_torreblanca,
-                                                                     mock_torrenegra, mock_cocupadas, setup_rey):
-        rey = setup_rey
+                                                                     mock_torrenegra, mock_cocupadas,
+                                                                     setup_teardown_rey,
+                                                                     setup_teardown_cocupadas):
+        rey = setup_teardown_rey
         torres_blancas = [0, MagicMock(movida=1), MagicMock(movida=0)]
         torres_negras = [0, MagicMock(movida=1), MagicMock(movida=1)]
         mock_torrenegra.__getitem__.side_effect = torres_negras.__getitem__
         mock_torreblanca.__getitem__.side_effect = torres_blancas.__getitem__
 
-        mock_cocupadas = [[0] * 8 for _ in range(8)]
+        matriz_cocupadas = setup_teardown_cocupadas
+        mock_cocupadas.__getitem__.side_effect = matriz_cocupadas.__getitem__
         mock_cocupadas[rey.casy][rey.casx + 1] = 1  # Marcamos como ocupada la casilla
 
         # Precondicion: Las torres en el lado izquierdo del tablero se movieron, las torres en el lado derecho y el
@@ -170,21 +189,22 @@ class TestMovrey:
         assert rey.movida == 0
         assert mov_posibles == []
         assert rey.casposibles == []
-
+    #
     @mock.patch('ajedrezoo.cocupadas')
     @mock.patch('ajedrezoo.torrenegra')
     @mock.patch('ajedrezoo.torreblanca')
     @mock.patch('ajedrezoo.metapieza.movlineal', return_value=[])
     @mock.patch('ajedrezoo.metapieza.movdiagonal', return_value=[])
     def test_3_casillas_izq_ocupada_y_enroque_derecha(self, mock_movlineal, mock_diagonal, mock_torreblanca,
-                                                      mock_torrenegra, mock_cocupadas, setup_rey):
-        rey = setup_rey
+                                                      mock_torrenegra, mock_cocupadas, setup_teardown_rey,
+                                                      setup_teardown_cocupadas):
+        rey = setup_teardown_rey
         torres_blancas = [0, MagicMock(movida=1), MagicMock(movida=0)]
         torres_negras = [0, MagicMock(movida=0), MagicMock(movida=1)]
         mock_torrenegra.__getitem__.side_effect = torres_negras.__getitem__
         mock_torreblanca.__getitem__.side_effect = torres_blancas.__getitem__
 
-        matriz_cocupadas = [[0] * 8 for _ in range(8)]
+        matriz_cocupadas = setup_teardown_cocupadas
         mock_cocupadas.__getitem__.side_effect = matriz_cocupadas.__getitem__
         mock_cocupadas[rey.casy][rey.casx - 3] = 1  # Marcamos como ocupada la casilla
 
@@ -213,15 +233,15 @@ class TestMovrey:
     @mock.patch('ajedrezoo.metapieza.movlineal', return_value=[])
     @mock.patch('ajedrezoo.metapieza.movdiagonal', return_value=[])
     def test_torres_izq_movidas(self, mock_movlineal, mock_diagonal, mock_torreblanca,
-                                mock_torrenegra, mock_cocupadas, setup_rey):
-        rey = setup_rey
+                                mock_torrenegra, mock_cocupadas, setup_teardown_rey, setup_teardown_cocupadas):
+        rey = setup_teardown_rey
         torres_blancas = [0, MagicMock(movida=1), MagicMock(movida=0)]
         torres_negras = [0, MagicMock(movida=1), MagicMock(movida=1)]
         mock_torrenegra.__getitem__.side_effect = torres_negras.__getitem__
         mock_torreblanca.__getitem__.side_effect = torres_blancas.__getitem__
 
         # Precondicion: Todas las casillas entre las torres y el rey estan libres
-        matriz_cocupadas = [[0] * 8 for _ in range(8)]
+        matriz_cocupadas = setup_teardown_cocupadas
         mock_cocupadas.__getitem__.side_effect = matriz_cocupadas.__getitem__
 
         # Precondicion: Las torres en el lado izquierdo del tablero se movieron, las torres en el lado derecho y el
@@ -245,15 +265,16 @@ class TestMovrey:
     @mock.patch('ajedrezoo.metapieza.movlineal', return_value=[])
     @mock.patch('ajedrezoo.metapieza.movdiagonal', return_value=[])
     def test_ninguna_torre_movida_casilla_izq_ocupada(self, mock_movlineal, mock_diagonal, mock_torreblanca,
-                                                      mock_torrenegra, mock_cocupadas, setup_rey):
-        rey = setup_rey
+                                                      mock_torrenegra, mock_cocupadas, setup_teardown_rey,
+                                                      setup_teardown_cocupadas):
+        rey = setup_teardown_rey
         torres_blancas = [0, MagicMock(movida=0), MagicMock(movida=0)]
         torres_negras = [0, MagicMock(movida=1), MagicMock(movida=1)]
         mock_torrenegra.__getitem__.side_effect = torres_negras.__getitem__
         mock_torreblanca.__getitem__.side_effect = torres_blancas.__getitem__
 
         # Precondicion: La casilla en la posicion [casx-3] esta ocupada
-        matriz_cocupadas = [[0] * 8 for _ in range(8)]
+        matriz_cocupadas = setup_teardown_cocupadas
         mock_cocupadas.__getitem__.side_effect = matriz_cocupadas.__getitem__
         mock_cocupadas[rey.casy][rey.casx - 3] = 1  # Marcamos como ocupada la casilla
         assert mock_cocupadas[rey.casy][rey.casx - 3] == 1
@@ -278,15 +299,16 @@ class TestMovrey:
     @mock.patch('ajedrezoo.metapieza.movlineal', return_value=[])
     @mock.patch('ajedrezoo.metapieza.movdiagonal', return_value=[])
     def test_ninguna_torre_movida_casilla_2_izq_ocupada(self, mock_movlineal, mock_diagonal, mock_torreblanca,
-                                                        mock_torrenegra, mock_cocupadas, setup_rey):
-        rey = setup_rey
+                                                        mock_torrenegra, mock_cocupadas, setup_teardown_rey,
+                                                        setup_teardown_cocupadas):
+        rey = setup_teardown_rey
         torres_blancas = [0, MagicMock(movida=0), MagicMock(movida=0)]
         torres_negras = [0, MagicMock(movida=1), MagicMock(movida=1)]
         mock_torrenegra.__getitem__.side_effect = torres_negras.__getitem__
         mock_torreblanca.__getitem__.side_effect = torres_blancas.__getitem__
 
         # Precondicion: La casilla en la posicion [casx-2] esta ocupada
-        matriz_cocupadas = [[0] * 8 for _ in range(8)]
+        matriz_cocupadas = setup_teardown_cocupadas
         mock_cocupadas.__getitem__.side_effect = matriz_cocupadas.__getitem__
         mock_cocupadas[rey.casy][rey.casx - 2] = 1  # Marcamos como ocupada la casilla
         assert mock_cocupadas[rey.casy][rey.casx - 2] == 1
@@ -311,15 +333,16 @@ class TestMovrey:
     @mock.patch('ajedrezoo.metapieza.movlineal', return_value=[])
     @mock.patch('ajedrezoo.metapieza.movdiagonal', return_value=[])
     def test_ninguna_torre_movida_casilla_1_izq_ocupada(self, mock_movlineal, mock_diagonal, mock_torreblanca,
-                                                        mock_torrenegra, mock_cocupadas, setup_rey):
-        rey = setup_rey
+                                                        mock_torrenegra, mock_cocupadas, setup_teardown_rey,
+                                                        setup_teardown_cocupadas):
+        rey = setup_teardown_rey
         torres_blancas = [0, MagicMock(movida=0), MagicMock(movida=0)]
         torres_negras = [0, MagicMock(movida=1), MagicMock(movida=1)]
         mock_torrenegra.__getitem__.side_effect = torres_negras.__getitem__
         mock_torreblanca.__getitem__.side_effect = torres_blancas.__getitem__
 
         # Precondicion: La casilla en la posicion [casx-1] esta ocupada
-        matriz_cocupadas = [[0] * 8 for _ in range(8)]
+        matriz_cocupadas = setup_teardown_cocupadas
         mock_cocupadas.__getitem__.side_effect = matriz_cocupadas.__getitem__
         mock_cocupadas[rey.casy][rey.casx - 1] = 1  # Marcamos como ocupada la casilla
         assert mock_cocupadas[rey.casy][rey.casx - 1] == 1
@@ -344,15 +367,16 @@ class TestMovrey:
     @mock.patch('ajedrezoo.metapieza.movlineal', return_value=[])
     @mock.patch('ajedrezoo.metapieza.movdiagonal', return_value=[])
     def test_ninguna_torre_movida_ninguna_casilla_ocupada(self, mock_movlineal, mock_diagonal, mock_torreblanca,
-                                                          mock_torrenegra, mock_cocupadas, setup_rey):
-        rey = setup_rey
+                                                          mock_torrenegra, mock_cocupadas, setup_teardown_rey,
+                                                          setup_teardown_cocupadas):
+        rey = setup_teardown_rey
         torres_blancas = [0, MagicMock(movida=0), MagicMock(movida=0)]
         torres_negras = [0, MagicMock(movida=1), MagicMock(movida=1)]
         mock_torrenegra.__getitem__.side_effect = torres_negras.__getitem__
         mock_torreblanca.__getitem__.side_effect = torres_blancas.__getitem__
 
         # Precondicion: Ninguna casilla esta ocupada
-        matriz_cocupadas = [[0] * 8 for _ in range(8)]
+        matriz_cocupadas = setup_teardown_cocupadas
         mock_cocupadas.__getitem__.side_effect = matriz_cocupadas.__getitem__
 
         # Precondicion: Ninguna de las torres blancas han sido movidas
